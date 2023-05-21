b384d0978ffa6fe8fbc538d39b132822fbbd2f9d
Jacquet <you@example.com>
2023-05-21 12:11:02 -0300

    push of (requisito funcionais)

---
+++ model/cliente.java
@@ -0,0 +1,25 @@
+package br.edu.projeto.model;
+
+public class cliente {
+
+    private String nome;
+
+    private String nomeSocial;
+    
+    private String cpf;
+    
+    private Integer altura;
+
+    private Integer massa;
+    
+    private String genero;
+    
+    private Integer idade;
+    
+    private String email;
+    
+    private String telefone;
+    
+    private String celular;
+
+}
